dc3c62e17eb6e0fd32327cfec221fc8f8d8fca55
Joy Lu <joylu1308@gmail.com>
2024-09-27 09:15:38 -0500

    Started writing test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,5 +1,5 @@
-public static string aryToString (int[] nums) {
-  string ans = "["
+public static String aryToString (int[] nums) {
+  String ans = "[";
   for (int i=0; i<nums.length; i++) {
     ans = ans + nums[i];
     if (i < nums.length - 1) {
@@ -7,4 +7,13 @@ public static string aryToString (int[] nums) {
     }
   }
   return ans;
+}
+
+
+public static void main (String args[]) {
+  int[] = a;
+
+   // test case aryToString:
+   a=[1, 3, 7];
+   System.out.println("Expected "[1, 3, 7]" +" vs my function: "+ aryToString[a];
 }
