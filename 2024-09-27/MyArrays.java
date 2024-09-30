f38797e0cb3eb8d9a7d23ac3959805d6641d9fc9
Joy Lu <joylu1308@gmail.com>
2024-09-29 21:13:56 -0400

    Finished writing and testing returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -12,6 +12,20 @@ public static String aryToString (int[] nums) {
   return ans;
 }
 
+public static int[] returnCopy(int[]ary){
+  int[] copy = new int[ary.length];
+  int i = 0;
+  for (int num : ary){
+    copy[i] = num;
+    i++;
+  }
+  return copy;
+}
+
+// public static int[] concatArray(int[]ary1,int[]ary2){
+
+//}
+
 public static void main (String args[]) {
   int[] a = new int[] {1, 3, 7};
   int[] b = new int[] {44, 3, 8};
@@ -23,13 +37,13 @@ public static void main (String args[]) {
    System.out.println("Expected [] vs my function: " + aryToString(c));
 
    // test case returnCopy:
-   System.out.println("Expected [1, 3, 7] vs my function: " + returnCopy(a));
-   System.out.println("Expected [44, 3, 8] vs my function: " + returnCopy(b));
-   System.out.println("Expected [] vs my function: " + returnCopy(c));
+   System.out.println("Expected [1, 3, 7] vs my function: " + aryToString(returnCopy(a)));
+   System.out.println("Expected [44, 3, 8] vs my function: " + aryToString(returnCopy(b)));
+   System.out.println("Expected [] vs my function: " + aryToString(returnCopy(c)));
 
    // test case concatArray:
-   System.out.println("Expected [1, 3, 7] vs my function: " + concatArray(a, c));
-   System.out.println("Expected [44, 3, 8, 1, 3, 7] vs my function: " + concatArray(b, a));
-   System.out.println("Expected [44, 3, 8] vs my function: " + concatArray(c, b));
+   //System.out.println("Expected [1, 3, 7] vs my function: " + concatArray(a, c));
+/*   System.out.println("Expected [44, 3, 8, 1, 3, 7] vs my function: " + concatArray(b, a));
+   System.out.println("Expected [44, 3, 8] vs my function: " + concatArray(c, b)); */
 }
 }
