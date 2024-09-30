8cdc3112d16fdaec1d5513cdd19ae34d2358ec1b
Joy Lu <joylu1308@gmail.com>
2024-09-29 20:23:33 -0400

    Added test cases for concatArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -27,5 +27,9 @@ public static void main (String args[]) {
    System.out.println("Expected [44, 3, 8] vs my function: " + returnCopy(b));
    System.out.println("Expected [] vs my function: " + returnCopy(c));
 
+   // test case concatArray:
+   System.out.println("Expected [1, 3, 7] vs my function: " + concatArray(a, c));
+   System.out.println("Expected [44, 3, 8, 1, 3, 7] vs my function: " + concatArray(b, a));
+   System.out.println("Expected [44, 3, 8] vs my function: " + concatArray(c, b));
 }
 }
