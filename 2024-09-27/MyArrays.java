b9f311809e6e5d63a5b84fd99618075f799f2dd1
Joy Lu <joylu1308@gmail.com>
2024-09-29 20:21:39 -0400

    Added test cases for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -12,11 +12,20 @@ public static String aryToString (int[] nums) {
   return ans;
 }
 
-
 public static void main (String args[]) {
   int[] a = new int[] {1, 3, 7};
+  int[] b = new int[] {44, 3, 8};
+  int[] c = new int[] {};
 
    // test case aryToString:
    System.out.println("Expected [1, 3, 7] vs my function: " + aryToString(a));
+   System.out.println("Expected [44, 3, 8] vs my function: " + aryToString(b));
+   System.out.println("Expected [] vs my function: " + aryToString(c));
+
+   // test case returnCopy:
+   System.out.println("Expected [1, 3, 7] vs my function: " + returnCopy(a));
+   System.out.println("Expected [44, 3, 8] vs my function: " + returnCopy(b));
+   System.out.println("Expected [] vs my function: " + returnCopy(c));
+
 }
 }
