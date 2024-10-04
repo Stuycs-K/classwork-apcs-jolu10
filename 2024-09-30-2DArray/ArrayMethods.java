d3b9039c2f23ecce91448d6f3f5acf54ad5d0041
Joy Lu <joylu1308@gmail.com>
2024-10-03 21:16:14 -0400

    Wrote and tested replaceNegative

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -83,7 +83,6 @@ public class ArrayMethods {
 //that negative with the value 1
 //-All other negatives replace with 0
 public static void replaceNegative(int[][] vals){
-  int[][] result = new int[vals[0].length][vals.length];
   for (int i = 0; i < vals.length; i++) {
     for (int n = 0; n < vals[i].length; n++) {
       if (vals[i][n] < 0) {
@@ -94,9 +93,8 @@ public static void replaceNegative(int[][] vals){
           vals[i][n] = 0;
         }
       }
-}
-}
-      return result;
+    }
+  }
 }
 
 //4. Make a copy of the given 2d array.
@@ -126,31 +124,36 @@ public static void replaceNegative(int[][] vals){
     System.out.println("Expected: [[3, 4], [4, 4, 3], [6], [9, 7]] vs my function: " + arrToString(ary7));
 
     // test cases for arr2DSum
-    System.out.println("Expected: 45 vs my function: " + arr2DSum(ary1));
+    System.out.println("Expected: 23 vs my function: " + arr2DSum(ary1));
     System.out.println("Expected: 49 vs my function: " + arr2DSum(ary2));
     System.out.println("Expected: 0 vs my function: " + arr2DSum(ary3));
     System.out.println("Expected: 0 vs my function: " + arr2DSum(ary6));
     System.out.println("Expected: 40 vs my function: " + arr2DSum(ary7));
 
     // test cases for swapRC
-    System.out.println("Expected: [[1, 4, 7], [2, 5, 8], [3, 6, -9]] vs my function: " + arrToString(swapRC(ary1)));
+    System.out.println("Expected: [[1, 4, 7], [-2, 5, 8], [3, 6, -9]] vs my function: " + arrToString(swapRC(ary1)));
     System.out.println("Expected: [[4, 5, 9], [3, 6, 4], [2, 7, 3], [1, 3, 2]] vs my function: " + arrToString(swapRC(ary2)));
     System.out.println("Expected: [[0, 5, 8], [3, -6, 5]] vs my function: " + arrToString(swapRC(ary4)));
     System.out.println("Expected: [[4, 8], [5, 9], [6, 0], [2, 1]] vs my function: " + arrToString(swapRC(ary5)));
     System.out.println("Expected: [] vs my function: " + arrToString(swapRC(ary6)));
 
     // test cases for replaceNegative
-    System.out.println("Expected: [[1, 0, 3], [4, 5, 6], [7, 8, -1]] vs my function: " + replaceNegative(ary1));
-    System.out.println("Expected: [[], [], [0, 0, 0]] vs my function: " + replaceNegative(ary3));
-    System.out.println("Expected: [[0, 3], [5, -1], [8, 5]] vs my function: " + replaceNegative(ary4));
-    System.out.println("Expected: [] vs my function: " + replaceNegative(ary6));
-    System.out.println("Expected: [[[-1, 0, 0], [0, -1, 0], [0, 0, -1]]] vs my function: " + replaceNegative(ary7));
+    replaceNegative(ary1);
+    replaceNegative(ary3);
+    replaceNegative(ary4);
+    replaceNegative(ary6);
+    replaceNegative(ary8);
+    System.out.println("Expected: [[1, 0, 3], [4, 5, 6], [7, 8, 1]] vs my function: " + arrToString(ary1));
+    System.out.println("Expected: [[], [], [0, 0, 0]] vs my function: " + arrToString(ary3));
+    System.out.println("Expected: [[0, 3], [5, 1], [8, 5]] vs my function: " + arrToString(ary4));
+    System.out.println("Expected: [[], [], [], []] vs my function: " + arrToString(ary6));
+    System.out.println("Expected: [[[1, 0, 0], [0, 1, 0], [0, 0, 1]]] vs my function: " + arrToString(ary8));
 
     // test cases for copy
     System.out.println("Expected: [[1, -2, 3], [4, 5, 6], [7, 8, -9]] vs my function: " + copy(ary1));
     System.out.println("Expected: [[4, 3, 2, 1], [5, 6, 7, 3], [9, 4, 3, 2]] vs my function: " + copy(ary2));
     System.out.println("Expected: [[], [], [0, 0, 0]] vs my function: " + copy(ary3));
     System.out.println("Expected: [[], [], [], []] vs my function: " + copy(ary6));
-    System.out.println("Expected: [[-4, -5, -6], [-1, -2, -3], [-7, -8, -9] vs my function: " + copy(ary8));
+    System.out.println("Expected: [[-4, -5, -6], [-1, -2, -3], [-7, -8, -9] vs my function: " + copy(ary8)); 
   }
 }
