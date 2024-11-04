01d0c86ce124fd7e1ae6e55ada7b7ab38d088ebc
Joy Lu <joylu1308@gmail.com>
2024-11-04 09:02:10 -0600

    Tested ReadFile.java to print every other line of text

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -10,8 +10,9 @@ public class ReadFile {
     try {
       File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
-      while (input.hasNext()){
-        System.out.println(input.next());
+      while (input.hasNextLine()){
+        input.nextLine();
+        System.out.println(input.nextLine());
       }
       //CODE THAT SCANS THE FILE.
       input.close();//releases the file from your program
