c9c2b700a1059ff693205b1e87a0377b9cb98848
Joy Lu <joylu1308@gmail.com>
2024-11-06 02:15:27 -0500

    Wrote and tested countTrianglesA

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TriangleTester {
+
+public static int countTrianglesA(String filename) {
+  int result = 0;
+  try {
+    File file = new File(filename);
+    Scanner scanner = new Scanner(file);
+    Scanner line;
+    int[] side = new int[3];
+    while(scanner.hasNextLine()){
+      line = new Scanner(scanner.nextLine());
+      for (int i = 0; i < 3; i++){
+        if (line.hasNext()){
+          side[i] = line.nextInt();
+        }
+      }
+      if (testTriangle(side[0], side[1], side[2])) {
+        result++;
+      }
+    }
+    scanner.close();
+  } catch (FileNotFoundException e) {
+    System.out.println("File not found");
+  }
+  return result;
+}
+
+public static boolean testTriangle (int side0, int side1, int side2) {
+  return ((side0 + side1 > side2) && (side1 + side2 > side0) && (side2 + side0 > side1));
+}
+
+public static void main(String[] args) {
+  System.out.println(countTrianglesA("inputTri.txt"));
+}
+
+public static int countTrianglesB(String filename) {
+  int result = 0;
+  try {
+    File file = new File(filename);
+    Scanner scanner = new Scanner(file);
+  }
+}
+}
