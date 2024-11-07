b7fcc0633cc946238ac5e68c861623664f1bcb57
Joy Lu <joylu1308@gmail.com>
2024-11-07 09:12:06 -0600

    Answered questions from step 8

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+// In Driver.java answer: What is the difference before and after you override this method? What does this tell us about methods that are overridden?
+// Before, getName() just returned name but after overriding this method, it returns "The Mighty" + super.getName().
+//This tells us that the new method is the one that's used instead of the original method.
 public class Driver {
   public static void main(String[] args) {
     Animal a1 = new Animal("Moo", 14, "Bob");
