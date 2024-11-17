dfdd351031cefa25d31a26b5ecd41500bf244530
Joy Lu <joylu1308@gmail.com>
2024-11-16 23:25:29 -0500

    Wrote replaceEmpty method and tested

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -3,8 +3,16 @@ import java.util.ArrayList;
 public class Driver{
   public static void main(String[] args) {
     ArrayList<String> arr0 = ArrayListPractice.createRandomArray(10);
-    System.out.println(arr0);
-    ArrayList<String> arr1 = ArrayListPractice.createRandomArray(20000);
-    System.out.println(arr1);
+    System.out.println("Array 0: " + arr0);
+    ArrayList<String> arr1 = ArrayListPractice.createRandomArray(200);
+    System.out.println("Array 1: " + arr1);
+
+    ArrayListPractice.replaceEmpty(arr0);
+    ArrayListPractice.replaceEmpty(arr1);
+    System.out.println("arr0 modified to replace empty strings: " + arr0);
+    System.out.println("arr1 modified to replace empty strings: " + arr1);
+
+    
+
   }
 }
