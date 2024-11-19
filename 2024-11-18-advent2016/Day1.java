541a44a87a5ebb568e3ea9aec2a26e10a9358c3d
Joy Lu <joylu1308@gmail.com>
2024-11-19 10:14:44 -0500

    Made input text file, wrote a bit of day1 method

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,12 +1,31 @@
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Day1 {
-  
-
+  public static int shortestPath(String filename) {
+    int blocks = 0;
+    try {
+      File file = new File(filename);
+      Scanner scanner = new Scanner(file);
+      Scanner line;
+      while(scanner.hasNextLine()){
+        line = new Scanner(scanner.nextLine());
+        for (int i = 0; i < 3; i++){
+          if (line.hasNext()){
+            side[i] = line.nextInt();
+          }
+        }
+        }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found.");
+    }
+    return blocks;
+  }
 
 public static void main(String[] args) {
-
-
+  System.out.println(shortestPath("inputDay1.txt"));
 }
 
 }
