4de00cda9a4341f7f2133ac25fc2f60ef0c16e0a
Joy Lu <joylu1308@gmail.com>
2024-11-22 09:21:40 -0500

    Worked on day 1 part 1

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -4,28 +4,46 @@ import java.util.Scanner;
 
 public class Day1 {
   public static int shortestPath(String filename) {
-    int blocks = 0;
+    int x = 0;
+    int y = 0;
+    int dir = 0;
     try {
       File file = new File(filename);
       Scanner scanner = new Scanner(file);
       Scanner line;
       while(scanner.hasNextLine()){
         line = new Scanner(scanner.nextLine());
-        for (int i = 0; i < 3; i++){
-          if (line.hasNext()){
-            side[i] = line.nextInt();
+        value = new Scanner(scanner.nextInt());
+          if (line.charAt(i) == 'L'){
+            dir = dir - 90;
           }
+          else if (line.charAt(i) == 'R'){
+              dir = dir + 90;
+            }
+          dir = (dir + 360) % 360;
+        }
+        if (dir == 0) {
+          y = y + value;
+        }
+        else if (dir == 90) {
+          x = x + value;
+        }
+        else if (dir == 180) {
+          y = y - value;
+        }
+        else if (dir == 270) {
+          x = x - value;
         }
         }
       scanner.close();
     } catch (FileNotFoundException e) {
       System.out.println("File not found.");
     }
-    return blocks;
+    return (Math.abs(x) + Math.abs(y));
   }
 
 public static void main(String[] args) {
-  System.out.println(shortestPath("inputDay1.txt"));
+  System.out.println(shortestPath("inputDay1.txt") + " should return 5.");
 }
 
 }
