654f841910183aebea7e3252c6301a65bb25a1e0
Joy Lu <joylu1308@gmail.com>
2024-12-04 09:56:11 -0500

    Color

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -2,26 +2,25 @@ public class ColorDemo {
   public static final String CLEAR_SCREEN =  "\u001b[2J";
   public static final String HIDE_CURSOR =  "\u001b[?25l";
   public static final String SHOW_CURSOR =  "\u001b[?25h";
-  public static int BLACK = 30;
-  public static int RED = 31;
+  public static int BLACK = 38;
+  public static int RED = 35;
 
   public static void color(int foreground,int background){
     System.out.print("\u001b[" + foreground + ";" + (10+background) + "m");
   }
 
   public static void color(int foreground, int background, int modifier){
-    System.out.print("\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+    System.out.print("\u001b[" + foreground + ";" + (100+background) + ";" + modifier + "m");
   }
   public static void main(String[] args) {
-    for(int r = 0; r < 256; r+=32){
-      for(int g = 0; g <= 256; g+=32){
-        for(int b = 0; b <= 256; b+=32){
+    for(int r = 0; r < 256; r+=12){
+      for(int g = 0; g <= 256; g+=17){
+        for(int b = 0; b <= 256; b+=15){
           System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
         }
       }
       System.out.println();
     }
-    System.out.print(CLEAR_SCREEN);
     System.out.print(HIDE_CURSOR);
   }
 }
