5fc714b68b584025a9a4c13c0b62a8ddfab07973
Joy Lu <joylu1308@gmail.com>
2024-12-08 01:01:33 -0500

    Wrote accessor methods

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,11 +1,28 @@
 public class Wizard implements Adventurer{
+  private int mana;
+  private int maxMana;
+
   public Wizard(String name){
-    this(name, 10);
+    super(name, 10);
   }
   public Wizard(String name, int hp){
-    this.name = name;
-    this.HP = hp;
-    this.maxHP = hp;
+    super(name, hp);
+  }
+
+  public String getSpecialName(){
+    return "Hocus Pocus";
+  }
+
+  public int getSpecial() {
+    return this.mana;
+  }
+
+  public void getSpecial(int n) {
+    this.mana = n;
+  }
+
+  public int getSpecialMax() {
+    return maxMana;
   }
 
   public static void main(String[] args){
