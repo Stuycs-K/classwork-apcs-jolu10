1ea6bf4306109aae94fb5fd6b3c938daca8dd2fd
Joy Lu <joylu1308@gmail.com>
2024-12-09 02:29:25 -0500

    Wrote wizard subclass and tested

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,32 @@
 public class Driver {
   public static void main(String[] args){
-    Adventurer p1 = new Wizard("Merlin", 10);
+    Adventurer p1 = new Wizard("Merlin");
+    Adventurer p2 = new Wizard("Harry", 70);
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println("Should return Hocus Pocus: " + p1.getSpecialName());
+    System.out.println("Should return Hocus Pocus: " + p2.getSpecialName());
+
+    System.out.println(p1.getName() + " has " + p1.getHP() + " HP.");
+    System.out.println(p2.getName() + " has " + p2.getHP() + " HP.");
+
+    System.out.println("Should return 20: " + p2.getHP());
+    System.out.println("Should return 20: " + p2.getHP());
+
+    System.out.println(p1.attack(p2));
+    System.out.println(p2.attack(p1));
+    System.out.println(p1.specialAttack(p2));
+    System.out.println(p2.specialAttack(p1));
+    System.out.println(p1.support(p2));
+    System.out.println(p1.support());
+    System.out.println(p2.support(p2));
+    System.out.println(p2.support());
+    System.out.println(p1.attack(p2));
+    System.out.println(p1.attack(p2));
+    System.out.println(p1.attack(p2));
+    System.out.println(p1.attack(p2));
+    System.out.println(p2.attack(p1)); // should not work because HP is 0
+    System.out.println(p2.specialAttack(p1));
+    System.out.println(p2.support());
   }
 }
--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,12 +1,14 @@
-public class Wizard implements Adventurer{
+public class Wizard extends Adventurer{
   private int mana;
-  private int maxMana;
+  private int maxMana = 100;
 
   public Wizard(String name){
-    super(name, 10);
+    super(name, 100);
+    this.mana = 50;
   }
   public Wizard(String name, int hp){
     super(name, hp);
+    this.mana = 50;
   }
 
   public String getSpecialName(){
@@ -14,18 +16,69 @@ public class Wizard implements Adventurer{
   }
 
   public int getSpecial() {
-    return this.mana;
+    return mana;
   }
 
-  public void getSpecial(int n) {
-    this.mana = n;
+  public void setSpecial(int n) {
+    mana = n;
   }
 
   public int getSpecialMax() {
     return maxMana;
   }
 
-  public static void main(String[] args){
+  //hurt or hinder the target adventurer
+  public String attack(Adventurer other) {
+    if (this.getHP() > 0) {
+      if (this.mana >= 5) {
+        other.setHP(Math.max(0, (other.getHP() - 15)));
+        this.setSpecial(Math.max(0, this.mana - 5));
+        return other.getName() + " was attacked by " + this.getName() + ". " + other.getName() + "'s HP is now " + other.getHP() + ". " + this.getName() + "'s mana is now " + this.mana + ".";
+      }
+      else {
+        return this.getName() + " can not attack. Insufficient mana.";
+      }
+    }
+    else {
+      return this.getName() + " can not attack. HP is 0.";
+    }
+}
 
+  //heall or buff the target adventurer
+  public String support(Adventurer other) {
+    if (this.getHP() > 0) {
+    other.setHP(other.getHP() + 10);
+    return other.getName() + " was healed by " + this.getName() + ". " + other.getName() + "'s HP is now " + other.getHP() + ".";
   }
+  else {
+    return this.getName() + " can not support. HP is 0.";
+  }
+}
+
+  //heall or buff self
+  public String support() {
+    if (this.getHP() > 0) {
+    this.setHP(this.getHP() + 10);
+    return this.getName() + " healed themself. " + this.getName() + "'s HP is now " + this.getHP() + ".";
+  }
+  else {
+    return this.getName() + " can not support. HP is 0.";
+  }
+}
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public String specialAttack(Adventurer other) {
+    if (this.getHP() > 0) {
+    if (this.mana > 10) {
+      other.setHP(Math.max(0, (other.getHP() - 30)));
+      return this.getName() + " used special attack on " + other.getName() + "! " + other.getName() + "'s HP is now " + other.getHP() + ".";
+    }
+    else {
+      return "Insufficient mana for special attack.";
+    }
+  }
+  else {
+    return this.getName() + " can not use special attack. HP is 0.";
+  }
+}
 }
