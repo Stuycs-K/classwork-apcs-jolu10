bcf54cd1e06ae1efcc32026acf6d984d891ebd1d
Joy Lu <joylu1308@gmail.com>
2025-01-03 10:15:55 -0500

    Started makeWords

--- 2025-01-02-fibonacci/Fib.java
+++ 2025-01-02-fibonacci/Fib.java
@@ -8,6 +8,17 @@ public class Fib {
     }
   }
 
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if (remainingLetters < 0) {
+      return "";
+    }
+    else {
+      for (int i = 0; i < alphabet.length(); i++) {
+        result = result + alphabet.substring(1);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     System.out.println(fibSequence(46)); // 46 is the largest n that calculates under 10 seconds
   }
